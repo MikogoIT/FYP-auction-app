4f0f52df116c8003e49846a25fcf2a4c1c922703
mahsunon <83772240+boiledsteak@users.noreply.github.com>
2025-04-16 22:51:33 +0800

    add dashboard page for testing

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+const Dashboard = () => {
+    return (
+        <div>
+            <h2 className="dashboardBox">
+                This is the dashboard page
+            </h2>
+        </div>
+    );
+
+};
+
+export default Dashboard;
