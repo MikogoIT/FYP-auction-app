bde87b48ed344113ed1fb2fccf7d49dbcd3dfeaa
boiledsteak <timothymxa@gmail.com>
2025-05-22 15:05:40 +0800

    try cloud runs

--- main.tf
+++ main.tf
@@ -8,29 +8,89 @@ terraform {
     } 
   }
   required_providers {
-        aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
+        google = {
+      source  = "hashicorp/google"
+      version = "~> 6.35"
     }
   }
 }
 
 variable "region" {
-  description = "AWS Region"
+  description = "gcp Region"
   type        = string
-  default     = "ap-southeast-1"
+  default     = "asia-southeast1"
 }
 
-provider "aws" {
-  region = var.region
+variable "project_id" {
+  description = "Your GCP project ID"
+  type        = string
+  default     = "auctioneer-460605"
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+variable "image_url" {
+  description = "Docker Hub image URI"
+  type        = string
+  default     = "docker.io/boiledsteak/fyp-auction-app:latest"
 }
 
 
-resource "aws_s3_bucket" "cdn" {
-  bucket = "auctioneer-cdn"
-  force_destroy = true
 
-  tags = {
-    Name = "Auctioneer CDN"
+#————————————————————————————————————
+# Enable the Cloud Run API
+#————————————————————————————————————
+resource "google_project_service" "run_api" {
+  service = "run.googleapis.com"
+  project = var.project_id
+  disable_on_destroy = false
+}
+
+#————————————————————————————————————
+# actual cloud run definition
+#————————————————————————————————————
+resource "google_cloud_run_v2_service" "cloud_run_app" {
+  name     = auctioneer-app
+  location = var.region
+
+  template {
+    containers {
+      image = var.image_url
+      port = 4433
+      env {
+        name  = "DATABASE_URL"
+        value = var.DATABASE_URL
+      }
+
+      resources {
+        limits = {
+          cpu    = "1"      # 1 vCPU
+          memory = "512Mi"  # 512 MiB RAM
+        }
+      }
+    }
   }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+
+  # ensure we've enabled the API first
+  depends_on = [google_project_service.run_api]
+}
+
+
+
+#————————————————————————————————————
+# allow public access to website
+#————————————————————————————————————
+resource "google_cloud_run_v2_service_iam_member" "public_invoker" {
+  location = google_cloud_run_v2_service.cloud_run_app.location
+  name     = google_cloud_run_v2_service.cloud_run_app.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
 }
