3f7bb390aa56187d2d69132562e8d273f4b0a951
mahsunon <83772240+boiledsteak@users.noreply.github.com>
2025-06-12 15:55:12 +0800

    activate pipeline

--- backend/server.js
+++ backend/server.js
@@ -92,7 +92,7 @@ const bucket = storage.bucket("auctioneer-dp-images");
 const allowedMimeTypes = ["image/jpeg", "image/png", "image/webp"];
 const upload = multer({
   storage: multer.memoryStorage(),
-  limits: { fileSize: 2 * 1024 * 1024 }, // 2MB max file size lor
+  limits: { fileSize: 2 * 1024 * 1024 }, // 2MB max file size ok
   fileFilter: (req, file, cb) => {
     if (allowedMimeTypes.includes(file.mimetype)) {
       cb(null, true);
