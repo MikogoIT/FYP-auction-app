3212f264f1375c0fcbd9f414727fbe581e3b788f
xiaohu1324 <2293403866@qq.com>
2025-06-16 16:14:53 +0800

    change back

--- backend/server.js
+++ backend/server.js
@@ -15,32 +15,75 @@ import auctionRoutes from "./routes/auctionRoutes.js";
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-
+// Get __dirname in ES module
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-
+// Connect to Neon db
 const sql = neon(process.env.DATABASE_URL);
 
 // Serve static frontend
 app.use(express.static(path.join(__dirname, '../frontend', 'dist')));
 
-// Enable JSON parsing
+// Enable JSON body parsing in advance
 app.use(express.json());
 
-// === API ROUTES ===
-app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/listings", listingRoutes);
+//listing router
+app.use("/api", listingRoutes);
+
+//bid router
 app.use("/api/bids", bidRoutes);
+
+// category router
 app.use("/api/categories", categoryRoutes);
+
+// auction router
 app.use("/api/auctions", auctionRoutes);
 
-// === React SPA fallback ===
-app.get("*", (req, res) => {
+
+// 
+
+// // Example API route that queries Neon DB
+// app.get("/api/version", async (req, res) => {
+//   try {
+//     const result = await sql`SELECT version()`;
+//     res.json({ version: result[0].version });
+//   } catch (error) {
+//     console.error("Database error:", error);
+//     res.status(500).json({ error: "Database error" });
+//   }
+// });
+
+
+// testing route 123
+// Example API route that queries Neon DB
+// app.get("/api/getAllUsers", async (req, res) => {
+//   try {
+//     const result = await sql`SELECT * FROM users`;
+//     res.json(result);
+//   } catch (error) {
+//     console.error("Database error:", error);
+//     res.status(500).json({ error: "Database error" });
+//   }
+// });
+
+//-------------------TEST Login--------------------//
+
+app.use("/api", authRoutes);      
+app.use("/api/users", userRoutes);
+app.use("/api", userRoutes); 
+
+//---------------------Test end--------------------//
+
+// Testing Database - UserModel
+app.get("/api/users", userRoutes);
+
+// Fallback to React app
+app.get("*name", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
 });
 
+// Start server
 app.listen(PORT, () => {
   console.log(`✅ Server running at http://localhost:${PORT}`);
 });
