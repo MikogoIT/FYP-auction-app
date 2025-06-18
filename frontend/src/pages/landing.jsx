f3874d742011d26389999f893732f51e8e8ce2de
mahsunon <83772240+boiledsteak@users.noreply.github.com>
2025-06-19 00:49:27 +0800

    try new button

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,28 +1,41 @@
+import '@material/web/button/filled-button.js';
 import { IMG_BASE_URL } from "../global-vars.jsx";
 
 export default function Profile() {
-    return (
-        
-        <div className="landingContent">
-            <div className="headerSpacing"></div>
-                <div className="theBlocks">
-                <div className="block" id="landingBlock1">
-                    <div className="title">
-                        <div>
-                            Auctioneer
-                        </div>
-                    </div>
-                    <div className="subtext">
-                        <div>
-                            Your Auctions, One Telegram Away
-                        </div>
-                        
-                    </div>
-                </div>
-                <div className="block" id="landingBlock2">
-                    <img src={`${IMG_BASE_URL}tele-wireframe-01.png`} style={{ height: "200px", cursor: "pointer" }}/>
-                </div>
-            </div>
-        </div>  
-    )
+  const handleGetStarted = () => {
+    // TODO: wire up your navigation or action here
+    console.log('Get Started clicked');
+  };
+
+  return (
+    <div className="landingContent">
+      <div className="headerSpacing" />
+      <div className="theBlocks">
+        <div className="block" id="landingBlock1">
+          <div className="title">
+            <div>Auctioneer</div>
+          </div>
+          <div className="subtext" style={{ textAlign: 'center' }}>
+            <div>Your Auctions, One Telegram Away</div>
+
+            {/* Material Web "filled" button */}
+            <md-filled-button
+              style={{ marginTop: '24px' }}
+              onClick={handleGetStarted}
+            >
+              Get Started
+            </md-filled-button>
+          </div>
+        </div>
+
+        <div className="block" id="landingBlock2">
+          <img
+            src={`${IMG_BASE_URL}tele-wireframe-01.png`}
+            style={{ height: "200px", cursor: "pointer" }}
+            alt="Telegram wireframe"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
