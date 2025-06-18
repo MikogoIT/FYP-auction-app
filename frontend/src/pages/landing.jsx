d8851fd925e031bb27de0f742ec794851d4e60ee
mahsunon <83772240+boiledsteak@users.noreply.github.com>
2025-06-19 00:23:27 +0800

    update landing page again

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -5,21 +5,25 @@ export default function Profile() {
         
         <div className="landingContent">
             <div className="headerSpacing"></div>
-            <div className="block">
-                <div className="title" id="subtext">
-                    <p>
-                        Auctioneer:
-                    </p>
-                    <p>
-                        Your Auctions, One Telegram Away
-                    </p>
+                <div className="theBlocks">
+                <div className="block">
+                    <div className="title">
+                        <div>
+                            Auctioneer
+                        </div>
+                    </div>
+                    <div className="subtext">
+                        <div>
+                            Your Auctions, One Telegram Away
+                        </div>
+                        <div>
+                            Manage auctions on the go: create listings, track bids and receive notifications in Telegram
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    Manage auctions on the go: create listings, track bids and receive notifications in Telegram
+                <div className="block" id="landingBlock2">
+                    <img src={`${IMG_BASE_URL}tele-wireframe-01.png`} style={{ height: "200px", cursor: "pointer" }}/>
                 </div>
-            </div>
-            <div className="block" id="landingBlock2">
-                <img src={`${IMG_BASE_URL}tele-wireframe-01.png`} style={{ height: "200px", cursor: "pointer" }}/>
             </div>
         </div>  
     )
