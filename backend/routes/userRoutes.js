88cec8656a8dfdffa716d73be5a88b5a0264f6d4
boiledsteak <timothymxa@gmail.com>
2025-06-19 17:05:48 +0800

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,14 +5,14 @@ import { requireLogin } from "../utils/requireLogin.js";
 
 const router = express.Router();
 
-router.get("/profile", userController.getProfile);
-router.put("/profile", userController.updateProfile);
-router.get("/admin/users", userController.getAllUsersController);
-router.put("/admin/freeze/:id", userController.toggleUserFreezeController);
-router.delete("/admin/delete/:id", userController.deleteUserController);
-router.get("/admin/search", userController.searchUsersController);
-router.get("/displayPhoto", userController.getDP);
-router.put("/displayPhoto", userController.uplDP);
+router.get("/profile", requireLogin, userController.getProfile);
+router.put("/profile", requireLogin, userController.updateProfile);
+router.get("/admin/users", requireLogin, userController.getAllUsersController);
+router.put("/admin/freeze/:id", requireLogin, userController.toggleUserFreezeController);
+router.delete("/admin/delete/:id", requireLogin, userController.deleteUserController);
+router.get("/admin/search", requireLogin, userController.searchUsersController);
+router.get("/displayPhoto",  requireLogin, userController.getDP);
+router.put("/displayPhoto", requireLogin, userController.uplDP);
 
 export default router;
 
