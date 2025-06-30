aa9614cd316ebabe55297fe87477af2fcd6cf7f2
boiledsteak <timothymxa@gmail.com>
2025-06-30 15:26:23 +0800

    Update main.py

--- telegram/main.py
+++ telegram/main.py
@@ -1,1 +1,7 @@
-print("yo this works")
+import functions_framework
+
+@functions_framework.http
+def hello_world(request):
+    # This will appear in your Cloud Function logs
+    print("Hello from Functions Framework!")
+    return ("Function executed, check logs for the print output.", 200)
