805ef201165dd2f85335d7320bd004d09f982226
Raysjz <raynor1997@gmail.com>
2025-07-03 15:42:33 +0800

    Updated Unique Constraint Per User

--- backend/models/feedbackModel.js
+++ backend/models/feedbackModel.js
@@ -4,5 +4,10 @@ export async function insertWebsiteFeedback(user_id, website_ratings, website_co
   return await sql`
     INSERT INTO website_feedback (user_id, website_ratings, website_comments)
     VALUES (${user_id}, ${website_ratings}, ${website_comments})
+    ON CONFLICT (user_id)
+    DO UPDATE SET
+      website_ratings = EXCLUDED.website_ratings,
+      website_comments = EXCLUDED.website_comments,
+      updated_at = NOW();
   `;
 }
