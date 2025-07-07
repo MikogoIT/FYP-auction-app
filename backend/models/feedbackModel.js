475c837786e8dcca2d5ccc190752b931ad3054ce
Raysjz <raynor1997@gmail.com>
2025-07-07 20:05:29 +0800

    Update feedbackModel.js

--- backend/models/feedbackModel.js
+++ backend/models/feedbackModel.js
@@ -32,7 +32,7 @@ export async function getAllWebsiteFeedback(sortOption = "latest") {
   }
 
   return await sql`
-    SELECT id, website_ratings, website_comments, created_at
+    SELECT user_id, website_ratings, website_comments, created_at
     FROM website_feedback
     ${orderByClause}
   `;
