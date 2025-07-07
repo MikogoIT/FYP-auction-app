5b614a5c149a04cb0fdb9de002f528c8a6fff293
mahsunon <83772240+boiledsteak@users.noreply.github.com>
2025-07-07 22:08:21 +0800

    fix squig

--- frontend/src/components/Squiggle.jsx
+++ frontend/src/components/Squiggle.jsx
@@ -2,28 +2,6 @@
 
 export default function Squiggle() {
   return (
-    <svg
-      width="100%"
-      height="8"
-      viewBox="0 0 91 8"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <defs>
-        <pattern
-          id="squigglePattern"
-          patternUnits="userSpaceOnUse"
-          width="91"
-          height="8"
-        >
-          <path
-            d="M114.4-5.067c-43.467 0-86.933 4.667-130.4 4.667-43.467 0-86.933-4.667-130.4-4.667s-86.933 4.667-130.4 4.667c-43.467 0-86.933-4.667-130.4-4.667"
-            stroke="#E1E3E1"
-            strokeLinecap="square"
-          />
-        </pattern>
-      </defs>
-      <rect width="100%" height="8" fill="url(#squigglePattern)" />
-    </svg>
+    <svg _ngcontent-cbq-c22="" aria-hidden="true" width="100%" height="8" fill="none" xmlns="http://www.w3.org/2000/svg"><pattern _ngcontent-cbq-c22="" id="a" width="91" height="8" patternUnits="userSpaceOnUse"><g _ngcontent-cbq-c22="" clip-path="url(#clip0_2426_11367)"><path _ngcontent-cbq-c22="" d="M114 4c-5.067 4.667-10.133 4.667-15.2 0S88.667-.667 83.6 4 73.467 8.667 68.4 4 58.267-.667 53.2 4 43.067 8.667 38 4 27.867-.667 22.8 4 12.667 8.667 7.6 4-2.533-.667-7.6 4s-10.133 4.667-15.2 0S-32.933-.667-38 4s-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0" stroke="#E1E3E1" stroke-linecap="square"></path></g></pattern><rect _ngcontent-cbq-c22="" width="100%" height="100%" fill="url(#a)"></rect></svg>
   );
 }
