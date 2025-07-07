ff01ced875caabfee1a36b143185122cde14584d
mahsunon <83772240+boiledsteak@users.noreply.github.com>
2025-07-07 21:55:40 +0800

    Update Squiggle.jsx

--- frontend/src/components/Squiggle.jsx
+++ frontend/src/components/Squiggle.jsx
@@ -1,4 +1,4 @@
-// src/components/Squiggle.js
+// src/components/Squiggle.jsx
 
 export default function Squiggle() {
   return (
@@ -9,23 +9,21 @@ export default function Squiggle() {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <pattern
-        id="squigglePattern"
-        patternUnits="userSpaceOnUse"
-        width="91"
-        height="8"
-      >
-        <path
-          d="M114.4-5.067c-43.467 0-86.933 4.667-130.4 4.667-43.467 0-86.933-4.667-130.4-4.667s-86.933 4.667-130.4 4.667c-43.467 0-86.933-4.667-130.4-4.667"
-          stroke="#E1E3E1"
-          strokeLinecap="square"
-        />
-      </pattern>
-      <rect
-        width="100%"
-        height="8"
-        fill="url(#squigglePattern)"
-      />
+      <defs>
+        <pattern
+          id="squigglePattern"
+          patternUnits="userSpaceOnUse"
+          width="91"
+          height="8"
+        >
+          <path
+            d="M114.4-5.067c-43.467 0-86.933 4.667-130.4 4.667-43.467 0-86.933-4.667-130.4-4.667s-86.933 4.667-130.4 4.667c-43.467 0-86.933-4.667-130.4-4.667"
+            stroke="#E1E3E1"
+            strokeLinecap="square"
+          />
+        </pattern>
+      </defs>
+      <rect width="100%" height="8" fill="url(#squigglePattern)" />
     </svg>
   );
 }
