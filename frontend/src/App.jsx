7b573ab828b30a18af23c0346ecfed46941c32c3
Raysjz <raynor1997@gmail.com>
2025-07-09 19:03:38 +0800

    Updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,6 +26,7 @@ import CategoryDetailPage from "./pages/CategoryDetailPage";
 import MyBids from "./pages/MyBids";
 import ContactPage from "./pages/ContactPage";
 import WatchedListings from "./pages/WatchedListings";
+import FeedbackForm from "./components/FeedbackForm";
 import FeedbackList from "./pages/FeedbackList";
 import UserFeedback from "./pages/UserFeedbackPage";
 import Notif from "./pages/Notif";
@@ -70,7 +71,7 @@ function AppRoutes({ isApiRoute }) {
             <Route path="/MyBids" element={<MyBids />} />
             <Route path="/Contact" element={<ContactPage />} />
             <Route path="/Watchlist" element={<WatchedListings />} />
-            <Route path="/feedback" element={<Feedback />} />
+            <Route path="/feedback" element={<FeedbackForm />} />
             <Route path="/feedbacklist" element={<FeedbackList />} />
             <Route path="/feedbackuser" element={<UserFeedback />} />
             <Route path="/notif" element={<Notif />} />
