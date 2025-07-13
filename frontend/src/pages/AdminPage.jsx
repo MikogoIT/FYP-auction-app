716e50c7f0f19637891961f3dea8516d751f9b74
Burd3n <103359731+Burd3n@users.noreply.github.com>
2025-07-13 22:18:42 +0800

    added more code from documentation

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -135,9 +135,21 @@ const AdminPage = () => {
     setRows(rows.filter((row) => row.id !== id));
   };
 
- const handleEditClick = (id) => () => {
-    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
+function EditToolbar(props) {
+  const { setRows, setRowModesModel } = props;
+
+  const handleClick = () => {
+    const id = randomId();
+    setRows((oldRows) => [
+      ...oldRows,
+      { id, username: '', email: '', isNew: true },
+    ]);
+    setRowModesModel((oldModel) => ({
+      ...oldModel,
+      [id]: { mode: GridRowModes.Edit, fieldToFocus: 'name' },
+    }));
   };
+};
 
   const currentPageUsers = users.slice((page - 1) * USERS_PER_PAGE, page * USERS_PER_PAGE);
   const totalPages = Math.ceil(users.length / USERS_PER_PAGE);
