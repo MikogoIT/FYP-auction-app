8d4a78d16421996a767ca2f6f047b7538795a498
Raysjz <raynor1997@gmail.com>
2025-07-16 15:52:32 +0800

    Fixing Routes

--- backend/routes/tagRoutes.js
+++ backend/routes/tagRoutes.js
@@ -6,6 +6,6 @@ import { requireLogin } from "../utils/requireLogin.js";
 const router = express.Router();
 
 router.post("/tag", requireLogin, tagController.insertTagsWithListing);
-router.get("/tag", tagController.getAllTags);
+router.get("/tag", tagController.fetchTagsForAutocomplete);
 
 export default router;
