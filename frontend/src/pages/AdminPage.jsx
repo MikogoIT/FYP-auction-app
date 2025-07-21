8e9c35116a5e94a7bec2eb2c43457fbab41ddafe
Burd3n <103359731+Burd3n@users.noreply.github.com>
2025-07-22 00:11:52 +0800

    adjusted buttons

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -287,7 +287,7 @@ const AdminPage = () => {
           }}
         >
           <Button variant="contained" color="#6750a4">User Management</Button>
-          <Button variant="contained" color="#e9def8">Category Management</Button>
+          <Button variant="outlined" color="#6750a4">Category Management</Button>
         </Box>
         <DataGrid 
           rows={users}
