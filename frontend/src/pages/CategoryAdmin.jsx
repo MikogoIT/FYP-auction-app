e801cbf33cf0928afc867e8fabfd455f85692755
Burd3n <103359731+Burd3n@users.noreply.github.com>
2025-07-22 16:25:00 +0800

    removed breadcrumbs nav

--- frontend/src/pages/CategoryAdmin.jsx
+++ frontend/src/pages/CategoryAdmin.jsx
@@ -3,7 +3,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-import BreadcrumbsNav from "../components/BreadcrumbsNav";
 
 
 // make sure you have these so <md-filled-button> and <md-filled-tonal-button> work
@@ -13,11 +12,15 @@ import "@material/web/button/filled-tonal-button.js";
 
 export default function CategoryAdmin() {
 
+  // need help to implement functions to:
+  // create category
+  // edit category
+  // suspend category
+
   return (
     <div className="dashboardCanvas">
       <div className="sidebarSpacer"></div>
       <div className="dashboardContent">
-        <BreadcrumbsNav />
         {/* page title */}
         <div className="profileTitle">Category Management</div>
         
