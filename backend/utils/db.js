665843e8586cff7d03d7615f454b85427e2a14b4
mahsunon <83772240+boiledsteak@users.noreply.github.com>
2025-07-22 23:36:30 +0800

    new db url

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -4,6 +4,7 @@ import { createClient } from '@supabase/supabase-js';
 const dbUrl = process.env.DATABASE_URL;
 if (!dbUrl) throw new Error('Missing SUPABASE_DB_URL');
 
+
 export const sql = createClient(dbUrl, {
 //   ssl: { rejectUnauthorized: false }
 });
